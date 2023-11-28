9f36c6db35af165e6bbed50e8f764115f276f577
kimyungi <dosa777@gmail.com>
2023-11-28 17:31:25 +0900

    update pro

--- lib/Sample/bottomsheet/bottom.dart
+++ lib/Sample/bottomsheet/bottom.dart
@@ -19,38 +19,36 @@ class test extends StatelessWidget {
   }
 }
 
-
 class test2 extends StatelessWidget {
-  const test2({super.key});
-
+  test2({super.key});
 
   void openBottomSheet() {
     Get.bottomSheet(
-        SizedBox(
-          height: 400,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'bottom sheet',
-                style: TextStyle(fontSize: 30),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: Get.back,
-                child: const Text('닫기'),
-              )
-            ],
-          ),
+      SizedBox(
+        height: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'bottom sheet',
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: Get.back,
+              child: const Text('닫기'),
+            )
+          ],
         ),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(20),
-            topLeft: Radius.circular(20),
-          ),
+      ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.circular(20),
         ),
-        clipBehavior: Clip.hardEdge,
-        backgroundColor: Colors.red,
+      ),
+      clipBehavior: Clip.hardEdge,
+      backgroundColor: Colors.red,
       isDismissible: false,
       enableDrag: false,
     );
@@ -67,20 +65,88 @@ class test2 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              child: Text('Show bottomsheet'),
-              onPressed: () {
-                openBottomSheet();
-              }
-            ),
-            ElevatedButton(onPressed: (){
-              showModalBottomSheet(context: context, builder: (context){
-                return SingleChildScrollView(
-                  child: Container(
-                    height: 500,
-                  ),
-                );
-              });
-            }, child: Text("showmodel sheet"))
+                child: Text('Show bottomsheet'),
+                onPressed: () {
+                  openBottomSheet();
+                }),
+            ElevatedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      builder: (context) {
+                        return SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.7,
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    alignment: Alignment.centerLeft,
+                                    height: 40,
+                                    child: const Text(
+                                      "테마",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Row(
+                                      children: [
+                                        squreBox("풍경"),
+                                        squreBox("인물"),
+                                        squreBox("정물"),
+                                        squreBox("동물"),
+                                        squreBox("추상"),
+                                        squreBox("팝아트"),
+                                        squreBox("오브제"),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 20,),
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    alignment: Alignment.centerLeft,
+                                    height: 40,
+                                    child: const Text(
+                                      "색상",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Row(
+                                      children: [
+                                        squreBox("풍경"),
+                                        squreBox("인물"),
+                                        squreBox("정물"),
+                                        squreBox("동물"),
+                                        squreBox("추상"),
+                                        squreBox("팝아트"),
+                                        squreBox("오브제"),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ));
+                      });
+                },
+                child: Text("showmodel sheet"))
           ],
         ),
       ),
@@ -88,3 +154,38 @@ class test2 extends StatelessWidget {
   }
 }
 
+Widget squreBox(String text) {
+  return Container(
+    height: 80,
+    width: 80,
+    alignment: Alignment.center,
+    margin: const EdgeInsets.only(left: 10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(5.0),
+      border: Border.all(color: Colors.grey),
+    ),
+    child: Text(
+      text,
+      style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    ),
+  );
+}
+
+Widget squreCircle(String text) {
+  return Container(
+    height: 80,
+    width: 80,
+    alignment: Alignment.center,
+    margin: const EdgeInsets.only(left: 10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(5.0),
+      border: Border.all(color: Colors.grey),
+    ),
+    child: Text(
+      text,
+      style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    ),
+  );
+}
