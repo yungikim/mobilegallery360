7c678e1832093e87cf2a6bd0de186a6979696841
dosa777 <dosa777@gmail.com>
2023-12-05 01:25:54 +0900

    Update Air

--- lib/pages/aboutUs/about_us.dart
+++ lib/pages/aboutUs/about_us.dart
@@ -19,6 +19,8 @@ class _AboutUsState extends State<AboutUs> {
   bool expand2 = false;
   bool expand3 = false;
 
+  final List<Step> _steps = getSteps();
+
   final images = [
     "ci_visual_acon.jpg",
     "ci_visual_acol.jpg",
@@ -29,6 +31,7 @@ class _AboutUsState extends State<AboutUs> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(60),
@@ -455,9 +458,52 @@ class _AboutUsState extends State<AboutUs> {
             ),
 
             SliverToBoxAdapter(
+              child: ExpansionPanelList(
+                expansionCallback: (int index, bool isExpanded){
 
+                  isExpanded = !isExpanded;
+                  setState(() {
+                    _steps[index].isExpanded = !isExpanded;
+                  });
+                },
+                children: _steps.map((Step step) {
+                  return ExpansionPanel(
+                    headerBuilder: (BuildContext context, bool isExpanded){
+                      return ListTile(
+                        title: Text(step.title),
+                      );
+                    },
+                    body: ListTile(
+                      title: Text(step.body),
+                    ),
+                    isExpanded: step.isExpanded,
+                  );
+                }).toList(),
+              ),
             ),
           ],
         ));
   }
 }
+
+
+class Step {
+  Step(
+      this.title,
+      this.body,
+      [this.isExpanded = false]
+      );
+  String title;
+  String body;
+  bool isExpanded;
+}
+
+List<Step> getSteps() {
+  return [
+    Step('Step 0: Install Flutter', 'Install Flutter development tools according to the official documentation.'),
+    Step('Step 1: Create a project', 'Open your terminal, run `flutter create <project_name>` to create a new project.'),
+    Step('Step 2: Run the app', 'Change your terminal directory to the project directory, enter `flutter run`.'),
+  ];
+}
+
+
