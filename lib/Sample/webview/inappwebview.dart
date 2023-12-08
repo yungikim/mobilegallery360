9ad3187ad453f16fd805284c0e80ad53f8cb8a3c
kimyungi <dosa777@gmail.com>
2023-12-08 13:34:06 +0900

    vscode

--- lib/Sample/webview/inappwebview.dart
+++ lib/Sample/webview/inappwebview.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +8,7 @@ import 'package:permission_handler/permission_handler.dart';
 
 import '../../const/const.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
 //  await Permission.camera.request();
@@ -25,13 +23,12 @@ void main() async{
   runApp(const MyApp());
 }
 
-void inx() async{
-  if (!kIsWeb){
+void inx() async {
+  if (!kIsWeb) {
     print("웹 서버 시작한다.");
-   // await localhostServer.start();
+    // await localhostServer.start();
   }
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -41,12 +38,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-
-
   @override
   Widget build(BuildContext context) {
-  //  inx();
+    //  inx();
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -55,24 +49,19 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
 class wapp extends StatefulWidget {
   const wapp({super.key});
-
-
 
   @override
   State<wapp> createState() => _wappState();
 }
 
-
 class _wappState extends State<wapp> {
-
   late InAppWebViewController webView;
 
   @override
-  void dispose(){
-   // localhostServer.close();
+  void dispose() {
+    // localhostServer.close();
     super.dispose();
   }
 
@@ -82,7 +71,7 @@ class _wappState extends State<wapp> {
       child: Scaffold(
         appBar: AppBar(
           leading: IconButton(
-            onPressed: (){
+            onPressed: () {
               Get.back();
             },
             icon: const Icon(Icons.arrow_back_ios),
@@ -90,52 +79,53 @@ class _wappState extends State<wapp> {
           title: const Text("Local Web"),
         ),
         //  appBar: AppBar(title: Text("In App Webview"),),
-        body:  InAppWebView(
-               //   initialFile: "assets/html/index.html",
-                  initialUrlRequest: URLRequest(
-                       url: Uri.parse('https://www.youtube.com/'),
-                   //   url: Uri.parse('https://www.gallery360.co.kr/main/vr_gallery/gallery360_vr_pfizer.jsp?key=gallery360@gallery360.co.kr_20191205095123_H2JHELW')
-                  ),
-
+        body: InAppWebView(
+          //   initialFile: "assets/html/index.html",
           // initialUrlRequest: URLRequest(
-          //   //   url: Uri.parse('http://localhost:8080/html/index.html?open&ver=1.0')
-          //     url: Uri.parse(
-          //         '$base_url/main/vr_gallery/gallery360_vr_pfizer.jsp?key=gallery360@gallery360.co.kr_20200828111648_QI2UPLR')),
-          //
-          //         onReceivedServerTrustAuthRequest: (controller, challenge) async {
-          //           print(challenge);
-          //           return ServerTrustAuthResponse(action: ServerTrustAuthResponseAction.PROCEED);
-          //         },
+          //      url: Uri.parse('https://www.youtube.com/'),
+          //  //   url: Uri.parse('https://www.gallery360.co.kr/main/vr_gallery/gallery360_vr_pfizer.jsp?key=gallery360@gallery360.co.kr_20191205095123_H2JHELW')
+          // ),
 
-                  androidOnPermissionRequest: (controller, origin, resources) async {
-                    return PermissionRequestResponse(
-                        resources: resources,
-                        action: PermissionRequestResponseAction.GRANT);
-                  },
+          initialUrlRequest: URLRequest(
+              //   url: Uri.parse('http://localhost:8080/html/index.html?open&ver=1.0')
+              url: Uri.parse(
+                  '$base_url/main/vr_gallery/gallery360_vr_pfizer.jsp?key=gallery360@gallery360.co.kr_20200828111648_QI2UPLR')),
 
-                  // initialOptions: InAppWebViewGroupOptions(
-                  //     android: AndroidInAppWebViewOptions(useHybridComposition: true),
-                  //     ios: IOSInAppWebViewOptions(
-                  //       allowsInlineMediaPlayback: true,
-                  //     ),
-                  //     crossPlatform: InAppWebViewOptions(
-                  //         supportZoom: false,
-                  //         preferredContentMode: UserPreferredContentMode.MOBILE,
-                  //         useShouldOverrideUrlLoading: true,
-                  //         mediaPlaybackRequiresUserGesture: false
-                  //     )
-                  // ),
+          onReceivedServerTrustAuthRequest: (controller, challenge) async {
+            print(challenge);
+            return ServerTrustAuthResponse(
+                action: ServerTrustAuthResponseAction.PROCEED);
+          },
+
+          androidOnPermissionRequest: (controller, origin, resources) async {
+            return PermissionRequestResponse(
+                resources: resources,
+                action: PermissionRequestResponseAction.GRANT);
+          },
+
+          // initialOptions: InAppWebViewGroupOptions(
+          //     android: AndroidInAppWebViewOptions(useHybridComposition: true),
+          //     ios: IOSInAppWebViewOptions(
+          //       allowsInlineMediaPlayback: true,
+          //     ),
+          //     crossPlatform: InAppWebViewOptions(
+          //         supportZoom: false,
+          //         preferredContentMode: UserPreferredContentMode.MOBILE,
+          //         useShouldOverrideUrlLoading: true,
+          //         mediaPlaybackRequiresUserGesture: false
+          //     )
+          // ),
 
           initialOptions: InAppWebViewGroupOptions(
             crossPlatform: InAppWebViewOptions(
-               transparentBackground: true,
-               allowFileAccessFromFileURLs: true,
-               useShouldOverrideUrlLoading: true,
-               allowUniversalAccessFromFileURLs: true,
-               javaScriptEnabled: true,
-               javaScriptCanOpenWindowsAutomatically: false,
+              transparentBackground: true,
+              allowFileAccessFromFileURLs: true,
+              useShouldOverrideUrlLoading: true,
+              allowUniversalAccessFromFileURLs: true,
+              javaScriptEnabled: true,
+              javaScriptCanOpenWindowsAutomatically: false,
               preferredContentMode: UserPreferredContentMode.MOBILE,
-                mediaPlaybackRequiresUserGesture: false,
+              mediaPlaybackRequiresUserGesture: false,
             ),
             ios: IOSInAppWebViewOptions(
               allowsInlineMediaPlayback: true,
@@ -146,31 +136,32 @@ class _wappState extends State<wapp> {
             ),
             android: AndroidInAppWebViewOptions(
               useHybridComposition: true,
-              mixedContentMode: AndroidMixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
+              mixedContentMode:
+                  AndroidMixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
             ),
           ),
 
-                  onWebViewCreated: (controller) {
-                    controller.addJavaScriptHandler(handlerName: 'handlerFoo', callback: (args) {
-                      // return data to the JavaScript side!
-                      return {
-                        'bar': 'bar_value', 'baz': 'baz_value'
-                      };
-                    });
+          onWebViewCreated: (controller) {
+            controller.addJavaScriptHandler(
+                handlerName: 'handlerFoo',
+                callback: (args) {
+                  // return data to the JavaScript side!
+                  return {'bar': 'bar_value', 'baz': 'baz_value'};
+                });
 
-                    controller.addJavaScriptHandler(handlerName: 'handlerFooWithArgs', callback: (args) {
-                      print(args[0]);
-                      return {"name" : "김윤기"};
-                      // it will print: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}]
-                    });
-                  },
-                  onConsoleMessage: (controller, consoleMessage) {
-                    print(consoleMessage.message);
-                    // it will print: {message: {"bar":"bar_value","baz":"baz_value"}, messageLevel: 1}
-                  },
-                ),
-
-
+            controller.addJavaScriptHandler(
+                handlerName: 'handlerFooWithArgs',
+                callback: (args) {
+                  print(args[0]);
+                  return {"name": "김윤기"};
+                  // it will print: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}]
+                });
+          },
+          onConsoleMessage: (controller, consoleMessage) {
+            print(consoleMessage.message);
+            // it will print: {message: {"bar":"bar_value","baz":"baz_value"}, messageLevel: 1}
+          },
+        ),
       ),
     );
   }
