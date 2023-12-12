7910c681355f25010113c0df82fb06de284fb8f9
kimyungi <dosa777@gmail.com>
2023-12-12 14:51:34 +0900

    update pro

--- android/app/src/main/kotlin/com/example/gallery360/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gallery360/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gallery360
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+//    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
