e82ed0c11fe5d192bd0b56e9c2d431ff653e5e33
dosa777 <dosa777@gmail.com>
2023-12-17 11:03:38 +0900

    com

--- android/app/src/main/kotlin/com/example/gallery360/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gallery360
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.SplashScreen
-
-class MainActivity: FlutterActivity() {
-//    override fun provideSplashScreen(): SplashScreen? = SplashView()
-}
