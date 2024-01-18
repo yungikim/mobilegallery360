d9c057c4cfc12b8b79e078780a626d7e4f8de639
kimyungi <dosa777@gmail.com>
2024-01-18 09:11:22 +0900

    update pro

--- lib/pages/MainPart/screen/widgets/spacebar6.dart
+++ lib/pages/MainPart/screen/widgets/spacebar6.dart
@@ -10,39 +10,40 @@ class SpaceBar6 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: ResponsiveBreakpoints.of(context).isDesktop ? 400 :  300,
+     // height: ResponsiveBreakpoints.of(context).isDesktop ? 400 :  300,
+      height: ResponsiveBreakpoints.of(context).isDesktop ? 200 :  150,
      // color: Colors.yellow,
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
-            const SizedBox(height: 10,),
-            Row(
-              children: [
-                Image.asset(
-                  "assets/images/mainPage/icon-event-01.png",
-                  width: 70,
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr('main_48'),
-                      style: TextStyle(
-                        fontSize: Util.fSize15,
-                      ),
-                    ),
-                    Text(
-                      tr('main_49'),
-                      style: TextStyle(fontSize: Util.fSize14, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            // const SizedBox(height: 10,),
+            // Row(
+            //   children: [
+            //     Image.asset(
+            //       "assets/images/mainPage/icon-event-01.png",
+            //       width: 70,
+            //     ),
+            //     const SizedBox(
+            //       width: 30,
+            //     ),
+            //     Column(
+            //       crossAxisAlignment: CrossAxisAlignment.start,
+            //       children: [
+            //         Text(
+            //           tr('main_48'),
+            //           style: TextStyle(
+            //             fontSize: Util.fSize15,
+            //           ),
+            //         ),
+            //         Text(
+            //           tr('main_49'),
+            //           style: TextStyle(fontSize: Util.fSize14, fontWeight: FontWeight.bold),
+            //         ),
+            //       ],
+            //     ),
+            //   ],
+            // ),
             const SizedBox(
               height: 10,
             ),
@@ -63,18 +64,23 @@ class SpaceBar6 extends StatelessWidget {
                 const SizedBox(
                   width: 30,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr('main_50'),
-                      style: TextStyle(fontSize: Util.fSize15),
-                    ),
-                    Text(
-                      tr('main_51'),
-                      style: TextStyle(fontSize: Util.fSize14, fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                GestureDetector(
+                  onTap: (){
+                    Util.UrlOpen("https://pf.kakao.com/_tAWTT/chat");
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr('main_50'),
+                        style: TextStyle(fontSize: Util.fSize15),
+                      ),
+                      Text(
+                        tr('main_51'),
+                        style: TextStyle(fontSize: Util.fSize14, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -86,31 +92,31 @@ class SpaceBar6 extends StatelessWidget {
               width: double.infinity,
               color: Colors.black.withOpacity(0.1),
             ),
-            const SizedBox(height: 10,),
-            Row(
-              children: [
-                Image.asset(
-                  "assets/images/mainPage/icon-event-03@2x.png",
-                  width: 70,
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr('main_52'),
-                      style: TextStyle(fontSize: Util.fSize15),
-                    ),
-                    Text(
-                      tr('main_53'),
-                      style: TextStyle(fontSize: Util.fSize14, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                )
-              ],
-            ),
+            // const SizedBox(height: 10,),
+            // Row(
+            //   children: [
+            //     Image.asset(
+            //       "assets/images/mainPage/icon-event-03@2x.png",
+            //       width: 70,
+            //     ),
+            //     const SizedBox(
+            //       width: 30,
+            //     ),
+            //     Column(
+            //       crossAxisAlignment: CrossAxisAlignment.start,
+            //       children: [
+            //         Text(
+            //           tr('main_52'),
+            //           style: TextStyle(fontSize: Util.fSize15),
+            //         ),
+            //         Text(
+            //           tr('main_53'),
+            //           style: TextStyle(fontSize: Util.fSize14, fontWeight: FontWeight.bold),
+            //         ),
+            //       ],
+            //     )
+            //   ],
+            // ),
           ],
         ),
       ),
